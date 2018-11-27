8babd1eab8b2f8072721e36ff030106272b87699
Brandon <BM848541@wcupa.edu>
2018-11-26 20:00:11 -0500

    Start Button
    Start button code for start menu "start game" button. Need admin of project (Carl) to add the following scenes to the build: Main, Segway2Game, StartScreen

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    public void StartGameButton (string newGame)
+    {
+        SceneManager.LoadScene(newGame);
+    }
+	
+}
