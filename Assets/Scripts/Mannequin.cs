e8dd50dda51714e69caeb4d1a8381f4530bc65df
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-12-08 16:38:26 -0500

    Mannequins/Mannequin AI
    * Added mannequins and a certain mannequin that moves!

---
+++ Assets/Scripts/Mannequin.cs
@@ -0,0 +1,28 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mannequin : MonoBehaviour {
+    public GameObject mannequinAI;
+    private Renderer render;
+    // Use this for initialization
+    void Start () {
+
+        render = this.GetComponent<SkinnedMeshRenderer>();
+    
+}
+	
+	// Update is called once per frame
+	void Update () {
+        if (render.isVisible)
+        {
+            mannequinAI.GetComponent<AILerp>().enabled= false;
+        }
+        else
+        {
+            mannequinAI.GetComponent<AILerp>().enabled = true;
+        }
+
+    }
+}
