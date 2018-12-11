e1bf0f89fd8e4fe67f2c1ac751a9a5faf3d1559d
Brandon <BM848541@wcupa.edu>
2018-12-10 19:14:14 -0500

    Added Audio Trigger On Zombie
    Zombie now has an invisible sphere that triggers a "scary soundtrack" when the player is within range of the monster. This acts as a warning when the zombie is close to an unwary player

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource BGM;
+
+    public void ChangeBGM(AudioClip music)
+    {
+        BGM.Stop();
+        BGM.clip = music;
+        BGM.Play();
+    }
+
+}
---
+++ Assets/Scripts/MusicTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour
+{
+
+    public AudioClip newTrack;
+    public AudioClip oldTrack;
+
+    private AudioManager theAM;
+
+    void Start()
+    {
+        theAM = FindObjectOfType<AudioManager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(newTrack != null)
+            {
+                theAM.ChangeBGM(newTrack);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (newTrack != null)
+            {
+                theAM.ChangeBGM(oldTrack);
+            }
+        }
+    }
+
+}
