7a99ab28510ad07311352888b6f1439d70475c14
Brandon <BM848541@wcupa.edu>
2018-12-12 23:45:24 -0500

    Menu Screens Work
    Start -> Segway -> Main Game -> (if you win) -> Win Screen
    -> (if you lose) -> lose screen

---
+++ Assets/Scripts/Start2But.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start2But : MonoBehaviour
+{
+
+    public void StartGameButton (string newGame)
+    {
+        SceneManager.LoadScene(newGame);
+    }
+	
+}
